ca9306e2d0f7efdcd71354b6345851226ac6e094
Katy Nicholson <62079949+CoasterKaty@users.noreply.github.com>
2024-10-07 17:38:18 +0100

    fix issue #20

--- www/oauth.php
+++ www/oauth.php
@@ -38,8 +38,7 @@ if ($sessionData) {
 	        echo $oAuth->errorMessage($reply->error_description);
 		exit;
 	}
-
-	$idToken = base64_decode(explode('.', $reply->id_token)[1]);
+	$idToken = base64_decode(str_replace("_", "/", str_replace("-", "+", explode('.', $reply->id_token)[1])));
 	$modDB->Update('tblAuthSessions', array('txtToken' => $reply->access_token, 'txtRefreshToken' => $reply->refresh_token, 'txtIDToken' => $idToken, 'txtRedir' => '', 'dtExpires' => date('Y-m-d H:i:s', strtotime('+' . $reply->expires_in . ' seconds'))), array('intAuthID' => $sessionData['intAuthID']));
 	// Redirect user back to where they came from.
 	header('Location: ' . $sessionData['txtRedir']);
