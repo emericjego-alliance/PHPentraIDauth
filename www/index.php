58d8ce80b5316c89257ddc234e0a2122f363994a
emericjego-alliance <emeric.jego@alliancemarine.com>
2024-10-18 16:57:10 +0200

    push1

--- www/index.php
+++ www/index.php
@@ -19,7 +19,7 @@ $Graph = new modGraph();
 $photo = $Graph->getPhoto();
 $profile = $Graph->getProfile();
 
-echo '<h1>Hello there, ' . $profile->displayName . ' (' . $Auth->userName . ')</h1>';
+echo '<h1>Hello Azure , ' . $profile->displayName . ' (' . $Auth->userName . ')</h1>';
 echo '<h2><a href="/?action=logout">Log out</a></h2>';
 echo 'Your roles in this app are:<ul>';
 foreach ($Auth->userRoles as $role) {
